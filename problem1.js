e60f9306ec1563cacfc69478ad0076bf1f461b66
Charles Clemens <cclemensjr@gmail.com>
2016-08-18 04:47:37 -0700

    Added more variables

--- problem1.js
+++ problem1.js
@@ -2,4 +2,13 @@
 //we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000.
 
+//the sum of all multiple
 var sum = 0;
+
+//We are searching for multiple up to this number
+var maxNum = 0;
+
+//We are searching for multiples of numbers
+const var multiple1 = 3;
+const var multiple2 = 5;
+
