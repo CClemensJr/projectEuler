19f1cbfca18652089c4dcf297a8c3e5158a43570
Charles Clemens <cclemensjr@gmail.com>
2016-08-19 04:25:25 -0700

    Added comments

---
+++ problem3.js
@@ -0,0 +1,6 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+//FROM THE ODIn PROJECT: just solve for numbers smaller than 1,000 and don't worry 
+//about making it efficient. You won't need to do any crazy math -- think iteration 
+//and make the computer do the work. Consider it extra credit to make your solution 
+//solve for larger numbers in a way that isn't too slow.
